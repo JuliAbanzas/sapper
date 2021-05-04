2aceb32692bc0f2f172bb7666cc049533c53b07c
JuliAbanzas <juliabanzas@gmail.com>
2021-05-04 16:53:14 -0300

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
+		border-bottom: 1px solid rgba(60, 255, 0, 0.1);
 		font-weight: 300;
 		padding: 0 1em;
 	}
@@ -50,8 +50,9 @@
 
 <nav>
 	<ul>
-		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
+		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>Home</a></li>
 		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+		<li><a class='{segment == "Login"?"selected":""}'href='login'>Login</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->

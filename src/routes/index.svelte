1e54f306e9456d6a3d041fa938671c576945eb4b
Julia Abanzas <juliabanzas@gmail.com>
2021-05-05 01:37:56 -0300

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -45,6 +45,19 @@
 	}
 
 	.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+	.button {
   transition-duration: 0.4s;
 }
 

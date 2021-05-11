0dfe50027eb522a1be96c661e5780b5af5c6d185
Julia Abanzas <juliabanzas@gmail.com>
2021-05-11 19:31:57 -0300

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,64 +1,186 @@
 <script>
-	export let segment;
-</script>
-
-<style>
+	import { onMount } from "svelte";
+  
+	// Show mobile icon and display menu
+	let showMobileMenu = false;
+  
+	// List of navigation items
+	const navItems = [
+	  { label: "logo", href: "#" },
+	  { label: "Item 1", href: "#" },
+	  { label: "Item 2", href: "#" },
+	  { label: "Item 3", href: "#" },
+	  { label: "Item 4", href: "#" },
+	  { label: "Item 5", href: "#" },
+	  { label: "Item 6", href: "#" },
+	  { label: "Item 7", href: "#" }
+	];
+  
+	// Mobile menu click event handler
+	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
+  
+	// Media match query handler
+	const mediaQueryHandler = e => {
+	  // Reset mobile state
+	  if (!e.matches) {
+		showMobileMenu = false;
+	  }
+	};
+  
+	// Attach media query listener on mount hook
+	onMount(() => {
+	  const mediaListener = window.matchMedia("(max-width: 767px)");
+  
+	  mediaListener.addListener(mediaQueryHandler);
+	});
+  </script>
+  
+  <nav>
+	<div class="inner">
+	  <div on:click={handleMobileIconClick} class={`mobile-icon${showMobileMenu ? ' active' : ''}`}>
+		<div class="middle-line"></div>
+	  </div>
+	  <ul class={`navbar-list${showMobileMenu ? ' mobile' : ''}`}>
+		{#each navItems as item}
+		  <li>
+			<a href={item.href}>{item.label}</a>
+		  </li>
+		{/each}
+	  </ul>
+	</div>
+  </nav>
+  
+  <style>
 	nav {
-		border-bottom: 1px solid rgba(60, 255, 0, 0.1);
-		font-weight: 300;
-		padding: 0 1em;
+	  background-color: rgba(0, 0, 0, 0.8);
+	  font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+	  height: 45px;
 	}
-
-	ul {
-		list-style-type: none;
-		margin: 0;
+  
+	.inner {
+	  max-width: 980px;
+	  padding-left: 20px;
+	  padding-right: 20px;
+	  margin: auto;
+	  box-sizing: border-box;
+	  display: flex;
+	  align-items: center;
+	  height: 100%;
+	}
+  
+	.mobile-icon {
+	  width: 25px;
+	  height: 14px;
+	  position: relative;
+	  cursor: pointer;
+	}
+  
+	.mobile-icon:after,
+	.mobile-icon:before,
+	.middle-line {
+	  content: "";
+	  position: absolute;
+	  width: 100%;
+	  height: 2px;
+	  background-color: #fff;
+	  transition: all 0.4s;
+	  transform-origin: center;
+	}
+  
+	.mobile-icon:before,
+	.middle-line {
+	  top: 0;
+	}
+  
+	.mobile-icon:after,
+	.middle-line {
+	  bottom: 0;
+	}
+  
+	.mobile-icon:before {
+	  width: 66%;
+	}
+  
+	.mobile-icon:after {
+	  width: 33%;
+	}
+  
+	.middle-line {
+	  margin: auto;
+	}
+  
+	.mobile-icon:hover:before,
+	.mobile-icon:hover:after,
+	.mobile-icon.active:before,
+	.mobile-icon.active:after,
+	.mobile-icon.active .middle-line {
+	  width: 100%;
+	}
+  
+	.mobile-icon.active:before,
+	.mobile-icon.active:after {
+	  top: 50%;
+	  transform: rotate(-45deg);
+	}
+  
+	.mobile-icon.active .middle-line {
+	  transform: rotate(45deg);
+	}
+  
+	.navbar-list {
+	  display: none;
+	  width: 100%;
+	  justify-content: space-between;
+	  margin: 0;
+	  padding: 0 40px;
+	}
+  
+	.navbar-list.mobile {
+	  background-color: rgba(0, 0, 0, 0.8);
+	  position: fixed;
+	  display: block;
+	  height: calc(100% - 45px);
+	  bottom: 0;
+	  left: 0;
+	}
+  
+	.navbar-list li {
+	  list-style-type: none;
+	  position: relative;
+	}
+  
+	.navbar-list li:before {
+	  content: "";
+	  position: absolute;
+	  bottom: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 1px;
+	  background-color: #424245;
+	}
+  
+	.navbar-list a {
+	  color: #fff;
+	  text-decoration: none;
+	  display: flex;
+	  height: 45px;
+	  align-items: center;
+	  padding: 0 10px;
+	  font-size: 13px;
+	}
+  
+	@media only screen and (min-width: 767px) {
+	  .mobile-icon {
+		display: none;
+	  }
+  
+	  .navbar-list {
+		display: flex;
 		padding: 0;
-		overflow:hidden;
-		background-color: #333;
+	  }
+  
+	  .navbar-list a {
+		display: inline-flex;
+	  }
 	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
-
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-
-<nav>
-	<ul>
-		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>Home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
-		
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
-	</ul>
-</nav>
+  </style>
